c7d1f8e37801f9c5732e02364317ff4f93dec378
muhdmirzamz <muhdmirzamz@gmail.com>
2024-10-15 16:38:33 +0800

    Dashboard is now divided into 3 layers

--- cms-frontend-example/src/routes/Dashboard.js
+++ cms-frontend-example/src/routes/Dashboard.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+
+import '../styles/Dashboard.css';
+
 const Dashboard = () => {
+
+    const [blogPostText, setBlogText] = useState('')
+
+    const blogPostTextValueChanged = (event) => {
+        setBlogText(event.target.value)
+    }
+
+    const postBlogPost = () => {
+
+    }
+
     return (
-        <div>
-            <h1>DASHBOARD</h1>
+        <div className='dashboard-body'>
+
+            <div className='layered-div'>
+                <div className='layer layer-one'>
+                    vertical layer one
+                </div>
+                <div className='layer layer-two'>
+                    vertical layer two
+                </div>
+                <div className='layer layer-three'>
+                    vertical layer three
+                </div>
+            </div>
+
+            {/* <h1>DASHBOARD</h1>
+
+            <textarea className="blogpost-text-area" value={blogPostText} onChange={blogPostTextValueChanged}>
+                nvksnvksnv
+            </textarea>
+            <input type='button' onClick={postBlogPost} /> */}
         </div>
     )
 }
