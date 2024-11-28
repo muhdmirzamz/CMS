4f837d2b673cce4fa516bc218e0d3fa49213ed82
muhdmirzamz <muhdmirzamz@gmail.com>
2024-11-29 00:20:18 +0800

    log out button now in a separate component

--- cms-frontend-example/src/component/LogoutButton.js
+++ cms-frontend-example/src/component/LogoutButton.js
@@ -1,11 +1,17 @@
 import { useNavigate } from 'react-router-dom'; 
 
+import CookieManager from '../CookieManager';
+
 import '../styles/logoutbutton.css'
 
 const LogoutButton = () => {
     const navigate = useNavigate()
+    
+    const cookieManager = new CookieManager()
 
     const logOut = () => {
+        cookieManager.deleteCookie("username")
+
         navigate('/')
     }
 
